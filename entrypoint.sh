df31cff83187601531f5257e47255f0320072082
Geoff Twardokus <geoff.twardokus@mail.rit.edu>
2024-05-16 12:38:20 -0400

    This version runs the simulation and copies the results into a local folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 /root/veins-veins-5.2/bin/veins_launchd -vv -c sumo &
 cd /root/veins-veins-5.2/examples/veins
 ./run -u Cmdenv
-
+opp_scavetool x results/General-\#0.sca -F JSON -o results/General-\#0.json
+cp -r ./results /root/veins-results/$(date +'%Y%m%d-%H%M%S%Z')
